4f883c3de3f75e30a6f80e19a90a067fab6f69ea
Shawn Renner <shawn.renner@gmail.com>
2020-06-18 15:18:02 -0500

    deliver xml stream to console

--- MealPlanner.ConsoleApp/Program.cs
+++ MealPlanner.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using MealPlanner.Export;
 
 namespace MealPlanner.ConsoleApp
@@ -10,7 +11,14 @@ namespace MealPlanner.ConsoleApp
             Console.WriteLine("Hello World!");
 
             //Class1.Test1();
-            EvernoteExporter.CreateAndSaveNote(DateTime.Now, "Example Note", new string[] { "bananas", "apples", "charcoal" });
+            //EvernoteExporter.CreateAndSaveNote(DateTime.Now, "Example Note", new string[] { "apples", "bananas", "chocolate" });
+
+            var exporter = new EvernoteExporter();
+            var stream = exporter.CreateExportedNote(DateTime.Now, "My List", new string[] { "apples", "bananas", "chocolate" });
+
+            var streamReader = new StreamReader(stream);
+            string text = streamReader.ReadToEnd();
+            Console.WriteLine(text);
         }
     }
 }
--- MealPlanner.Export/EvernoteExporter.cs
+++ MealPlanner.Export/EvernoteExporter.cs
@@ -10,12 +10,7 @@ namespace MealPlanner.Export
     {
         public Stream CreateExportedNote(DateTime date, string title, string[] checklist)
         {
-            throw new NotImplementedException();
-        }
-
-        private static string CreateInnerNoteContents(string[] checklist)
-        {
-
+            string formattedDate = date.ToString("yyyyMMddTHHmmssZ");
 
             XmlDocument doc = new XmlDocument();
 
@@ -23,30 +18,75 @@ namespace MealPlanner.Export
             XmlElement root = doc.DocumentElement;
             doc.InsertBefore(xmlDeclaration, root);
 
-            XmlDocumentType xmlDoctype = doc.CreateDocumentType("en-note", null, "http://xml.evernote.com/pub/enml2.dtd", null);
+            XmlDocumentType xmlDoctype = doc.CreateDocumentType("en-export", null, "http://xml.evernote.com/pub/evernote-export2.dtd", null);
+
             doc.InsertAfter(xmlDoctype, xmlDeclaration);
 
+            XmlElement enExportNode = doc.CreateElement("en-export");
+            enExportNode.SetAttribute("export-date", formattedDate);
+            enExportNode.SetAttribute("application", "Evernote/MealPlanner");
+            enExportNode.SetAttribute("version", "6.x");
+
+            doc.AppendChild(enExportNode);
+
+            XmlElement noteNode = doc.CreateElement("note");
+            enExportNode.AppendChild(noteNode);
+
+            XmlElement titleNode = doc.CreateElement("title");
+            titleNode.InnerText = title;
+            noteNode.AppendChild(titleNode);
+
+            XmlElement contentNode = doc.CreateElement("content");
+            noteNode.AppendChild(contentNode);
+
+            string innerNoteContents = CreateInnerNoteContents(checklist);
+
+            XmlCDataSection cdata = doc.CreateCDataSection(innerNoteContents);
+            contentNode.AppendChild(cdata);
+
+            XmlElement createdNode = doc.CreateElement("created");
+            createdNode.InnerText = formattedDate;
+            noteNode.AppendChild(createdNode);
+
+            XmlElement updatedNode = doc.CreateElement("updated");
+            updatedNode.InnerText = formattedDate;
+            noteNode.AppendChild(updatedNode);
+
+            MemoryStream xmlStream = new MemoryStream();
+            doc.Save(xmlStream);
+            xmlStream.Position = 0;
+
+            return xmlStream;
+        }
+
+        private static string CreateInnerNoteContents(string[] checklist)
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlDeclaration xmlDeclaration = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            XmlElement root = doc.DocumentElement;
+            doc.InsertBefore(xmlDeclaration, root);
+            XmlDocumentType xmlDoctype = doc.CreateDocumentType("en-note", null, "http://xml.evernote.com/pub/enml2.dtd", null);
+            doc.InsertAfter(xmlDoctype, xmlDeclaration);
+            
             XmlElement noteNode = doc.CreateElement("en-note");
             doc.AppendChild(noteNode);
-
+            
             foreach(var item in checklist)
             {
                 XmlElement divNode = doc.CreateElement("div");
                 divNode.InnerText = item;
-
+                
                 XmlElement todoNode = doc.CreateElement("en-todo");
                 todoNode.SetAttribute("checked", "false");
                 
                 divNode.AppendChild(todoNode);
-
-
                 noteNode.AppendChild(divNode);
             }
-
             return doc.InnerXml;
         }
 
 
+        [Obsolete]
         public static void CreateAndSaveNote(DateTime datetime, string title, string[] checklist)
         {
             string formattedDate = datetime.ToString("yyyyMMddTHHmmssZ");
