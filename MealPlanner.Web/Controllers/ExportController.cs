1e255228d4e45457bea777ef1757631d587c0638
Shawn Renner <shawn.renner@gmail.com>
2020-06-19 10:15:28 -0500

    get export file from web

---
+++ MealPlanner.Web/Controllers/ExportController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MealPlanner.Export;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MealPlanner.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExportController : ControllerBase
+    {
+        [HttpGet]
+        public FileResult Get()
+        {
+            EvernoteExporter exporter = new EvernoteExporter();
+            var stream = exporter.CreateExportedNote(DateTime.Now, "Example streamed note", new string[] { "apples", "bananas", "coconuts", "dates", "elderberry" });
+            return File(stream, "application/xml", "export.enex");
+        }
+    }
+}
